b16c8543c6004a9291d38e3fc231e4ac517c4a9c
yannaw <yannaw@hotmail.com>
2017-06-01 16:58:31 +0200

    Add project files.

---
+++ FirstMVC/Controllers/WorkController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVC.Controllers
+{
+    public class WorkController : Controller
+    {
+        // GET: Work
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Work/Details/5
+        public ActionResult Details(int? idx, int? id)
+        {
+            ViewBag.Idx = idx;
+            ViewBag.Id = id;
+
+            return View();
+        }
+    }
+}
